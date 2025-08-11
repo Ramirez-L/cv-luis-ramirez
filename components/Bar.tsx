aed09d93a6accd72de3b15679a95dacb37fd6dca
Luis Ramirez <luisramirezn95@gmail.com>
2025-08-11 08:42:29 -0300

    updates

--- components/Bar.tsx
+++ components/Bar.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const logo = "https://ramirezluishomelab.com/main-logo-black.svg";
@@ -6,7 +7,7 @@ const Bar = () => {
   return (
     <nav className="bg-black py-6 px-52 sticky top-0 text-slate-100 justify-center content-center">
       <div>
-        <img src={logo} width={100} height={100} alt={""} />
+        <Image src={logo} width={100} height={100} alt={""} />
       </div>
     </nav>
   );
